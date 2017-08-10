3f00090ca247dfc963f7cf889ca08ba10c5f13c9
Nuno Rosa <rnunojanela@gmail.com>
2017-08-10 18:28:08 +0100

    Filter IPAddress by user

--- netbox/ipam/views.py
+++ netbox/ipam/views.py
@@ -601,6 +601,10 @@ class IPAddressListView(ObjectListView):
     table = tables.IPAddressDetailTable
     template_name = 'ipam/ipaddress_list.html'
 
+    def get(self, request):
+        self.queryset = self.queryset.filter_access(request.user)
+        return super(IPAddressListView, self).get(request)
+
 
 class IPAddressView(View):
 
