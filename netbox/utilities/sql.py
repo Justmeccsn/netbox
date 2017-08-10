5e5240ff38c8b121b0c53999c48a9450a2472eec
Nuno Rosa <rnunojanela@gmail.com>
2017-08-10 15:01:23 +0100

    Filter Racks by user

--- netbox/utilities/sql.py
+++ netbox/utilities/sql.py
@@ -32,3 +32,47 @@ class NullsFirstQuerySet(models.QuerySet):
     def __init__(self, model=None, query=None, using=None, hints=None):
         super(NullsFirstQuerySet, self).__init__(model, query, using, hints)
         self.query = query or NullsFirstQuery(self.model)
+
+
+class ObjectFilterQuerySet(models.QuerySet):
+    def filter_access(self, user):
+        if not user.is_superuser:
+            try:
+                return self.filter(tenant__users__in=user)
+            except TypeError:
+                return self.none()
+        return self
+
+
+class NaturalOrderByQuerySet(models.QuerySet):
+
+    def natural_order_by(self, *fields):
+        """
+        Attempt to order records naturally by segmenting a field into three parts:
+
+        1. Leading integer (if any)
+        2. Middle portion
+        3. Trailing integer (if any)
+
+        :param fields: The fields on which to order the queryset. The last field in the list will be ordered naturally.
+        """
+        db_table = self.model._meta.db_table
+        primary_field = fields[-1]
+
+        id1 = '_{}_{}1'.format(db_table, primary_field)
+        id2 = '_{}_{}2'.format(db_table, primary_field)
+        id3 = '_{}_{}3'.format(db_table, primary_field)
+
+        queryset = self.extra(select={
+            id1: "CAST(SUBSTRING({}.{} FROM '^(\d{{1,9}})') AS integer)".format(db_table, primary_field),
+            id2: "SUBSTRING({}.{} FROM '^\d*(.*?)\d*$')".format(db_table, primary_field),
+            id3: "CAST(SUBSTRING({}.{} FROM '(\d{{1,9}})$') AS integer)".format(db_table, primary_field),
+        })
+
+        ordering = fields[0:-1] + (id1, id2, id3)
+
+        return queryset.order_by(*ordering)
+
+
+class FilterNaturalOrderByQuerySet(ObjectFilterQuerySet, NaturalOrderByQuerySet):
+    pass
